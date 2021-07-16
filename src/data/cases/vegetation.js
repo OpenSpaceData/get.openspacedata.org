60db38686f06e8271a9deed18cbe8f93a94f8e7e
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:03:56 +0200

    Update vegetation cases with real content

--- src/data/cases/vegetation.js
+++ src/data/cases/vegetation.js
@@ -1,21 +1,16 @@
 export const vegetation = {
         id: 'vegetation',
-        intro: 'Choose one of the following options to analyze vegetation and forestry:',
+        intro: 'Choose one of the following options to analyze vegetation and geology:',
         options: [
             {
-                heading: 'Visualizing Barren Soil',
-                difficulty: 'easy',
-                task: 'visualize barren soil'
-            },
-            {
                 heading: 'Analyzing Green Vegetation Health',
-                difficulty: 'easy to moderate',
+                difficulty: 'easy',
                 task: 'analyze green vegetation health'
             },
             {
-                heading: 'Analyzing Land Cover',
-                difficulty: 'easy',
-                task: 'analyze land cover'
+                heading: 'Visualizing Barren Soil',
+                difficulty: 'moderate',
+                task: 'visualize barren soil'
             },
         ]
     }
