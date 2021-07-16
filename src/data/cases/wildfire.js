6b3e647fb5b31322bb135176c76034def0db4e1f
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:33:05 +0200

    Add wildfire cases and submit real content

---
+++ src/data/cases/wildfire.js
@@ -0,0 +1,11 @@
+export const wildfire = {
+        id: 'wildfire',
+        intro: 'Choose one of the following options to investigate wildfires:',
+        options: [
+            {
+                heading: 'Detect Burned Areas',
+                difficulty: 'easy',
+                task: 'detect burned areas'
+            },
+        ]
+    }
