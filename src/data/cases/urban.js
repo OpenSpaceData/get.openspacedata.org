2c553f257849e2e8b9ab2ac18d2859b9d750fc6b
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:32:52 +0200

    Add urban cases and submit real content

---
+++ src/data/cases/urban.js
@@ -0,0 +1,16 @@
+export const urban = {
+        id: 'urban',
+        intro: 'Choose one of the following options to check urban development:',
+        options: [
+            {
+                heading: 'True Color Image',
+                difficulty: 'easy',
+                task: 'get a true color image from'
+            },
+            {
+                heading: 'Visualizing Urbanized Areas',
+                difficulty: 'easy',
+                task: 'visualize urbanized areas'
+            },
+        ]
+    }
