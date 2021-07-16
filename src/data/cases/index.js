1e83ae7da9c56997522ea2587fa11449de779845
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:34:45 +0200

    Add the cases to the index

--- src/data/cases/index.js
+++ src/data/cases/index.js
@@ -1,5 +1,10 @@
 import {vegetation} from "./vegetation";
+import {flood} from "./flood";
+import {snow} from "./snow";
+import {urban} from "./urban";
+import {water} from "./water";
+import {wildfire} from "./wildfire";
 
 export const cases = [
-    vegetation
+    vegetation, flood, snow, urban, water, wildfire
 ]
