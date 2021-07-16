b1419f78b6cccab988bc778004da66041f21b41c
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:32:46 +0200

    Add snow cases and submit real content

---
+++ src/data/cases/snow.js
@@ -0,0 +1,11 @@
+export const snow = {
+        id: 'snow',
+        intro: 'Choose one of the following options to examine snow and glacier:',
+        options: [
+            {
+                heading: 'Detect Snow Cover',
+                difficulty: 'easy',
+                task: 'detect snow cover'
+            },
+        ]
+    }
