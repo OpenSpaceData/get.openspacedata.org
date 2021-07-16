91ead1b08542cb074b9fba73c2c243aae8ad2462
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:32:58 +0200

    Add water cases and submit real content

---
+++ src/data/cases/water.js
@@ -0,0 +1,11 @@
+export const water = {
+        id: 'water',
+        intro: 'Choose one of the following options to inspect ocean and water bodies:',
+        options: [
+            {
+                heading: 'Detect Water',
+                difficulty: 'easy',
+                task: 'detect water'
+            },
+        ]
+    }
