949e7f21e7d2cd8a84c9a74f2a26ac2624437677
Niklas Jordan <hello@niklasjordan.com>
2021-07-16 11:32:39 +0200

    Add flood cases and submit real content

---
+++ src/data/cases/flood.js
@@ -0,0 +1,16 @@
+export const flood = {
+        id: 'flood',
+        intro: 'Choose one of the following options to study floods and droughts:',
+        options: [
+            {
+                heading: 'Detect Water',
+                difficulty: 'easy',
+                task: 'detect water'
+            },
+            {
+                heading: 'Visualizing Barren Soil',
+                difficulty: 'moderate',
+                task: 'visualize barren soil'
+            },
+        ]
+    }
