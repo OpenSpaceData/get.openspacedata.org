c054263cbbbc5adddb407c1ba2769d908a5384fe
fershad <fershad@gmail.com>
2021-08-14 14:57:44 +0800

    add styles and content to about page

---
+++ src/lib/Follow.svelte
@@ -0,0 +1,26 @@
+<div>
+  For updates follow us on 🚀 <a href="https://twitter.com/openspacedata1">Twitter</a>, 📝
+  <a href="https://medium.com/openspacedata">Medium</a>
+  or 👩🏽‍💻 <a href="https://github.com/OpenSpaceData">GitHub</a>, or subscribe to our 📬
+  <a href="https://www.openspacedata.org/mailing-list/">mailing list</a>.
+</div>
+
+<style>
+  div {
+    background: var(--color-accent);
+    padding: 1rem;
+    border-radius: 2px;
+    color: var(--color-black);
+    font-size: var(--size-600);
+    text-align: center;
+  }
+
+  a,
+  a:visited {
+    background-image: linear-gradient(90deg, var(--color-accent), var(--color-accent)),
+      linear-gradient(90deg, var(--color-black), var(--color-black));
+    text-shadow: -1px -1px 0 var(--color-accent), 1px -1px 0 var(--color-accent),
+      -1px 1px 0 var(--color-accent), 1px 1px 0 var(--color-accent);
+    color: currentColor;
+  }
+</style>
