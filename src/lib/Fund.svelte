679084971cb89ca8add8afb5527889c0b11d8bc3
fershad <fershad@gmail.com>
2021-08-16 13:22:54 +0800

    add image component for Funds

--- src/lib/Fund.svelte
+++ src/lib/Fund.svelte
@@ -1,14 +1,18 @@
+<script>
+  import Image from '$lib/Image.svelte'
+</script>
+
 <div class="text-center">
   <h2>This project is funded by</h2>
   <div class="fluid-grid three">
     <a href="https://bmbf.de/en"
-      ><img
-        src="/img/fund/BMBF_gefoerdert_2017_en.svg"
+      ><Image
+        path="/img/fund/BMBF_gefoerdert_2017_en.svg"
         alt="Logo for German Federal Ministry of Education & Research" /></a>
     <a href="https://okfn.de/en"
-      ><img src="/img/fund/okfn.svg" alt="Logo for Open Knowledge Foundation Deutschland" /></a>
+      ><Image path="/img/fund/okfn.svg" alt="Logo for Open Knowledge Foundation Deutschland" /></a>
     <a href="https://prototypefund.de/en"
-      ><img src="/img/fund/PrototypeFund-P-Logo.svg" alt="Logo for Prototype Fund" /></a>
+      ><Image path="/img/fund/PrototypeFund-P-Logo.svg" alt="Logo for Prototype Fund" /></a>
   </div>
 </div>
 
