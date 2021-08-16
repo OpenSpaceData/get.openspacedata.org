8ae825bd37b3ba51badbf4a97acb202a39d1db37
fershad <fershad@gmail.com>
2021-08-16 12:21:31 +0800

    create really simple image element

---
+++ src/lib/Image.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let path
+  export let alt = ''
+  export let height
+  export let width
+
+  const src =
+    process.env.NODE_ENV === 'development'
+      ? path
+      : `https://res.cloudinary.com/itsfish/image/fetch/f_jpg,q_auto/https://openspacedata.netlify.app${path}`
+</script>
+
+<img {src} {alt} {height} {width} />
