b7924fb261fd6a5e7bc051ac87a1cb182d45bbc2
fershad <fershad@gmail.com>
2021-08-16 13:43:54 +0800

    add fetching data animation for form submit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,7 +26,7 @@
 </script>
 
 <script>
-  import {fly, slide} from 'svelte/transition'
+  import {fly, slide, fade} from 'svelte/transition'
   import {choice as selected, location, range} from '$lib/store'
   import NumberedHeading from '$lib/NumberedHeading.svelte'
   import ProgressHeading from '$lib/ProgressHeading.svelte'
@@ -36,6 +36,8 @@
   import Progress from '$lib/Progress.svelte'
   import {goto} from '$app/navigation'
   import {toast} from '@zerodevx/svelte-toast'
+  import InlineSVG from 'svelte-inline-svg'
+  import satellite from '$lib/svg/satellite.svg'
 
   export let categories
   export let cases
@@ -72,6 +74,7 @@
 
   let progress = 1
   let showProgress = true
+  let fetching = false
 
   $: $selected ? (progress = 2) : (progress = 1)
   $: $location ? (progress = 3) : (progress = 2)
@@ -158,32 +161,63 @@
           <TimeRange />
         </div>
       {/if}
-      <a
-        class="submit"
-        on:click|preventDefault={() => {
-          checkForm()
-          !disableButton
-            ? goto(
-                `/guide/${$selected.id}?location=${$location.bbox}&from=${rangeStart}&to=${rangeEnd}`
-              )
-            : null
-        }}
-        href="#"
-        class:disableButton
-        >Alright! Get the data and start analyzing
-        {#if !disableButton}
-          <!-- content here -->
+      {#if fetching}
+        <!-- content here -->
+        <div class="loading" out:fade|local={{duration: 50}} in:fade|local={{delay: 50}}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            class="icon icon-tabler icon-tabler-satellite"
+            width="100"
+            height="100"
             viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#000000"
             fill="none"
-            ><path
-              fill="currentColor"
-              d="M10.061 19.061L17.121 12 10.061 4.939 7.939 7.061 12.879 12 7.939 16.939z" /></svg>
-        {/if}
-      </a>
+            stroke-linecap="round"
+            stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M3.707 6.293l2.586 -2.586a1 1 0 0 1 1.414 0l5.586 5.586a1 1 0 0 1 0 1.414l-2.586 2.586a1 1 0 0 1 -1.414 0l-5.586 -5.586a1 1 0 0 1 0 -1.414z" />
+            <path d="M6 10l-3 3l3 3l3 -3" />
+            <path d="M10 6l3 -3l3 3l-3 3" />
+            <line x1="12" y1="12" x2="13.5" y2="13.5" />
+            <path d="M14.5 17a2.5 2.5 0 0 0 2.5 -2.5" />
+            <path d="M15 21a6 6 0 0 0 6 -6" />
+          </svg>
+          <h2>Fetching data ...</h2>
+        </div>
+      {:else}
+        <!-- else content here -->
+        <a
+          out:fade|local={{duration: 50}}
+          in:fade|local={{delay: 50}}
+          class="submit"
+          on:click|preventDefault={() => {
+            fetching = !fetching
+            checkForm()
+            !disableButton
+              ? goto(
+                  `/guide/${$selected.id}?location=${$location.bbox}&from=${rangeStart}&to=${rangeEnd}`
+                )
+              : null
+          }}
+          href="#"
+          class:disableButton
+          >Alright! Get the data and start analyzing
+          {#if !disableButton}
+            <!-- content here -->
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              ><path
+                fill="currentColor"
+                d="M10.061 19.061L17.121 12 10.061 4.939 7.939 7.061 12.879 12 7.939 16.939z" /></svg>
+          {/if}
+        </a>
+      {/if}
     </div>
   </div>
 </section>
@@ -241,5 +275,13 @@
     background: var(--color-dark-grey) !important;
     color: var(--color-white) !important;
     cursor: default !important;
+  }
+
+  .loading {
+    width: 100%;
+    display: flex;
+    gap: var(--size-600);
+    justify-content: center;
+    align-items: center;
   }
 </style>
