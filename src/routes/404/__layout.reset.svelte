ca1b840372856f775c4bd22caa16981bce4ec11d
Fershad <itsfish@fershad.com>
2021-08-30 11:17:35 +0800

    add 404 page

---
+++ src/routes/404/__layout.reset.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Header from '$lib/Header.svelte'
+  import Footer from '$lib/Footer.svelte'
+  import '../../app.css'
+</script>
+
+<svelte:head>
+  <title>OpenSpaceData | Cannot find page</title>
+</svelte:head>
+
+<Header />
+
+<main class="flow wrapper">
+  <slot />
+</main>
+
+<Footer />
+
+<style>
+  main.flow {
+    --flow-space: var(--size-500);
+  }
+</style>
